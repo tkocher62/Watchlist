a3e1fbc8a3c08f89e74b325a9a72b4df67abd058
Cyanox <Infernogaming621@gmail.com>
2020-02-22 19:51:27 -0500

    Added lookup

--- Watchlist/DataObjects/Identify.cs
+++ Watchlist/DataObjects/Identify.cs
@@ -1,9 +1,11 @@
-﻿namespace Watchlist.DataObjects
+﻿using GameCore;
+
+namespace Watchlist.DataObjects
 {
 	public class Identify
 	{
 		public string type = "IDENT";
 		public int data = ServerConsole.Port;
-		public int maxPlayers = 32;
+		public int maxUsers = ConfigFile.ServerConfig.GetInt("max_players");
 	}
 }
